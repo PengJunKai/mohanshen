73281133df414381be7da4117203bca3679770d0
644710708@qq.com <644710708vae>
2017-02-23 12:34:34 +0800

    按钮提醒

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by Peng on 2017/2/23.
  */
+$("#more").click(function() {
+   layer.msg("没有啦，她们还在设计，也可能没设计！");
+});
+
+$("#submit").click(function() {
+   layer.msg("后台都没有，你提交个蛋啊");
+});
