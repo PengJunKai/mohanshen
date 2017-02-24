55794835f677301b3d8edb856c12d73772b7e9d7
peng <644710708@qq.com>
2017-02-25 01:01:29 +0800

    顶部导航栏基本功能

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,7 @@ var _photoJson =
             "alt": "Friends",
             "pid": 2,
             "src": "http://www.mohansheng.com/photos/i-m-priscilla-149914-1062x708.jpg",
-            "thumb": "http://www.mohanshen.com/photos/i-m-priscilla-149914-1062x708.jpg"
+            "thumb": "http://www.mohansheng.com/photos/i-m-priscilla-149914-1062x708.jpg"
         },
         {
             "alt": "Father",
@@ -28,11 +28,14 @@ var _photoJson =
         {
             "alt": "Camera",
             "pid": 4,
-            "src": "http://www.mohansheng.com/6e26rfp0zbu-jacob-sapp-1062x708.jpg",
-            "thumb": "http://www.mohansheng.com/6e26rfp0zbu-jacob-sapp-1062x708.jpg"
+            "src": "http://www.mohansheng.com/photos/6e26rfp0zbu-jacob-sapp-1062x708.jpg",
+            "thumb": "http://www.mohansheng.com/photos/6e26rfp0zbu-jacob-sapp-1062x708.jpg"
         }
     ]
 };
+
+//数组存各区域到顶部的距离
+var _top = new Array;
 
 $("#more").click(function() {
    layer.msg("没有啦，她们还在设计，也可能没设计！");
@@ -41,15 +44,55 @@ $("#more").click(function() {
 $("#submit").click(function() {
    layer.msg("后台都没有，你提交个蛋啊");
 });
-
-$("#imageMenu,#designMenu").click(function () {
-    layer.msg("不存在的");
+$("#indexMenu").click(function () {
+    $("body").scrollTop(0);
+})
+$("#imageMenu").click(function () {
+    $("body").scrollTop(_top[0]);
 });
+
+$("#designMenu").click(function () {
+    $("body").scrollTop(_top[1]);
+});
+$("#messageMenu").click(function () {
+    $("body").scrollTop(_top[2]);
+})
 
 $("#aboutMenu").click(function () {
     layer.photos({
         photos: _photoJson
     });
+})
+
+$("#more").click(function () {
+
+});
+
+
+$(document).ready(function(){
+
+    _top[0] = $(".imageDiv").position().top-75;
+    _top[1] = $(".designDiv").position().top-75;
+    _top[2] = $(".messageDiv").position().top-75;
+
+    $(window).scroll(function () {
+        var winTop = $(window).scrollTop();
+        if(winTop<_top[0]) {
+            $("#indexMenu").addClass("underline");
+            $("#imageMenu,#designMenu,#messageMenu").removeClass("underline");
+        }
+        if(winTop >= _top[0] && winTop <_top[1]-1) {
+            $("#imageMenu").addClass("underline");
+            $("#indexMenu,#designMenu,#messageMenu").removeClass("underline");
+        } else if(winTop >= (_top[1]-1) && winTop <_top[2]-1) {
+            $("#designMenu").addClass("underline");
+            $("#indexMenu,#imageMenu,#messageMenu").removeClass("underline");
+        } else if(winTop >= _top[2]-1) {
+            $("#messageMenu").addClass("underline");
+            $("#indexMenu,#designMenu,#imageMenu").removeClass("underline");
+        }
+    });
+    　
 });
 
 
